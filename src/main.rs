dfdda2708900356c0a25e0ce003ec0630b4c9562
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-07-25 21:39:20 -0400

    add game stats printing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,62 @@
+use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Thanks for playing Rock Paper Scissors!");
+
+    let mut game_stats = GameStats::new();
+
+    let mut keep_playing = true;
+    while keep_playing == true {
+        let mut rounds_won = 0;
+        let mut rounds_lost = 0;
+        let mut rounds_tied = 0;
+
+        while rounds_won + rounds_lost + rounds_tied < 3 {
+            let player_choice = get_user_choice();
+            game_stats.update_player_choice_stats(&player_choice);
+
+            let ai_choice = get_ai_choice();
+
+            let round_summary = "You played " + &player_choice + " and the computer played " + &ai_choice;
+            let round_result = get_game_result(player_choice, ai_choice);
+
+            // update round stats and provide message
+            match round_result {
+                GAME_RESULT::Win => {
+                    rounds_won += 1;
+                    println!("{} - You won!", round_summary);
+                },
+                GAME_RESULT::Loss => {
+                    rounds_lost += 1;
+                    println!("{} - You lost!", round_summary);
+                },
+                GAME_RESULT::Tie => {
+                    rounds_tied += 1;
+                    println!("{} - You tied!", round_summary);
+                }
+            }
+        }
+
+        // calculate win / loss across rounds
+        if rounds_won > rounds_lost {
+            println!("CONGRATS! You won the game!");
+            game_stats.update_game_results(&GAME_RESULT::Win);
+        } else if rounds_lost > rounds_won {
+            println!("Oh no! You lost the game!");
+            game_stats.update_game_results(&GAME_RESULT::Loss);
+        } else {
+            println!("The game ended in a tie!");
+            game_stats.update_game_results(&GAME_RESULT::Tie);
+        }
+
+        // print current game stats
+
+        // ask player if they want to keep playing
+
+        println!("Would you like to keep playing - y / n ?")
+
+    }
 }
 
 // declare game enums
@@ -18,7 +73,17 @@ enum GAME_RESULT {
 }
 
 // stub out major functions
-fn get_user_choice() {}
+fn get_user_choice() -> RSP_CHOICE {
+    println!("Select: r - rock, p - paper, s - scissors");
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("failed to read user input");
+    match choice {
+        &"r" => RSP_CHOICE::Rock,
+        &"s" => RSP_CHOICE::Scissors,
+        &"p" => RSP_CHOICE::Paper,
+        _ => get_user_choice()
+    }
+}
 
 // randomly generate an RSP choice
 fn get_ai_choice() -> RSP_CHOICE {
@@ -79,7 +144,7 @@ impl GameStats {
         }
     }
 
-    pub fn update_game_results(&mut self, result: GAME_RESULT) {
+    pub fn update_game_results(&mut self, result: &GAME_RESULT) {
         match result {
             GAME_RESULT::Win => self.wins += 1,
             GAME_RESULT::Loss => self.losses += 1,
@@ -87,15 +152,25 @@ impl GameStats {
         }
     }
 
-    pub fn get_result_ratio(&self) -> i8 {
-        self.wins - self.losses
+    pub fn get_win_ratio(&self) -> i8 {
+        self.wins - (self.losses + self.ties)
     }
 
-    pub fn update_player_choice_stats(&mut self, choice: RSP_CHOICE ) {
+    pub fn update_player_choice_stats(&mut self, choice: &RSP_CHOICE ) {
         match choice {
             RSP_CHOICE::Rock => self.rock_picks += 1,
             RSP_CHOICE::Paper => self.paper_picks_picks += 1,
             RSP_CHOICE::Scissors => self.scissor_picks_picks += 1,
         }
+    }
+
+    pub fn print_game_stats(&self) {
+        println!("Wins: {}", self.wins);
+        println!("Losses: {}", self.losses);
+        println!("Ties: {}", self.ties);
+        println!("Win Ratio: {}%", self.get_win_ratio());
+        println!("Rock Picked: {} times", self.rock_picks);
+        println!("Paper Picked: {} times", self.paper_picks);
+        println!("Scissors Picked: {} times", self.scissor_picks);
     }
 }
